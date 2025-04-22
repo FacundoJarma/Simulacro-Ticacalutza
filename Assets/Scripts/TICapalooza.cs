a042dff28b905d4b1903e5329d4e25665de8891f
FacundoJarma <49127963@est.ort.edu.ar>
2025-04-22 08:50:12 -0300

    Terminado

--- Assets/Scripts/TICapalooza.cs
+++ Assets/Scripts/TICapalooza.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class TICapalooza : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int entradasCampoVendidas;
+    public int entradasPlateasVendidas;
+
+    int CapacidadPredio = 36600;
+    int CapacidadMaximaPlateas = 16200;
+    int CapacidadMaximaCampo = 20400;
+    int PrecioEntradaCampo = 1200;
+    int PrecioEntradaPlatea = 1200;
+
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(entradasCampoVendidas < 0 || entradasPlateasVendidas < 0 || entradasPlateasVendidas > CapacidadMaximaPlateas || entradasCampoVendidas > CapacidadMaximaCampo){
+            Debug.Log("[-] Ingresa una cantidad de entradas valida.");
+            return;
+        }
+
+        int recaudacionCampo = entradasCampoVendidas * PrecioEntradaCampo;
+        int recaudacionPlateas = entradasPlateasVendidas * PrecioEntradaPlatea;
+        Debug.Log("[!] En campo se recaudo un total de: " + recaudacionCampo + "$");
+        Debug.Log("[!] En plateas se recaudo un total de: " + recaudacionPlateas + "$");
+        Debug.Log("[!] En total se recaudo: " + (recaudacionPlateas + recaudacionCampo) + "$");
+
+        int totalVendidas = (entradasCampoVendidas + entradasPlateasVendidas);
+        int lugaresRestantes = CapacidadPredio - totalVendidas;
+        if(lugaresRestantes == 0){
+            Debug.Log("[!] Hubo sold out.");
+        }
+        else{
+            Debug.Log("[!] Quedaron vacantes: " + lugaresRestantes + " lugares.");
+        }
+
+        if(totalVendidas > CapacidadPredio/2){
+            Debug.Log("[+] El festival fue un exito!!");
+        }
+        else{
+            Debug.Log("[-] Debemos mejorar la convocatoria.");
+        }
+
     }
 }
